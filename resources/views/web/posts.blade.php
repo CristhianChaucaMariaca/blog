c596211bd148907181c8c0bf968108fadd5bf517
Cristhian Chauca <cristhiancchm+git@gmail.com>
2020-03-12 09:38:52 -0400

    Se arregla la vista de posts con bootstrap 4

--- resources/views/web/posts.blade.php
+++ resources/views/web/posts.blade.php
@@ -4,16 +4,24 @@
         <div class="col-md-8 col-md-offset-2">
             <h1>Lista de articulos</h1>
             @foreach ($posts as $post)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        {{$post->name}}
-                    </div>
-                    <div class="panel-body">
-                        @if ($post->file)
-                            <img src="{{$post->file}}" class="img-responsive"/>
-                        @endif
-                        {{$post->excerpt}}
-                        <a href="#" class="pull-right">Leer mas</a>
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="">
+                                {{$post->name}}
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="">
+                                @if ($post->file)
+                                    <img src="{{$post->file}}" class="img-fluid"/>
+                                @endif
+                                <p class="card-text">
+                                    {{$post->excerpt}}
+                                </p>
+                                <a href="#" class="pull-right">Leer mas</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
